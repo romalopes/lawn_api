2a92c19c520332ef91f5e668864224067a9981cf
romalopes <romalopes@yahoo.com.br>
2016-03-07 01:25:52 +1100

    travis

--- test.rb
+++ test.rb
@@ -9,17 +9,31 @@ def app
   Sinatra::Application
 end
 
-describe "Test hello world" do
+# describe "Test hello world" do
 
-def test_it_says_hello_world
-    get '/'
-    assert last_response.ok?
-    assert_equal 'Hello World', last_response.body
+# def test_it_says_hello_world
+#     get '/'
+#     assert last_response.ok?
+#     assert_equal 'Hello World', last_response.body
+#   end
+
+#   def test_it_says_hello_to_a_person
+#     get '/', :name => 'Simon'
+#     assert last_response.body.include?('Simon')
+#   end
+
+# end
+
+describe "Lawn" do
+  describe "GET /lawns" do
+    before do
+      # using the rack::test:methods, call into the sinatra app and request the following url
+      get "/lawns"
+    end
+
+    it "responds successfully" do
+      # Ensure the request we just made gives us a 200 status code
+      last_response.status.must_equal 200
+    end
   end
-
-  def test_it_says_hello_to_a_person
-    get '/', :name => 'Simon'
-    assert last_response.body.include?('Simon')
-  end
-
 end
